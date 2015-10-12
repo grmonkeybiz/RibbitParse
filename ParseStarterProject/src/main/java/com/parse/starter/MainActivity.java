9ea6e9d3397d9dc5750fef84d868586212693749
grmonkeybiz <grmonkeybiz@gmail.com>
2015-10-12 09:44:17 -0700

    Initial commit

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseUser;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+  public static final String TAG = MainActivity.class.getSimpleName();
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+    ParseUser currentUser = ParseUser.getCurrentUser();
+
+    if (currentUser == null) {
+        navigateToLogin();
+        }
+        else {
+            Log.i(TAG, currentUser.getUsername());
+
+             }
+
+
+        }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        // login in will created a new task and if we leave it will be cleared so we can exit the app.
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemIde = item.getItemId();
+
+        if(itemIde == R.id.action_logout){
+            ParseUser.logOut();
+            navigateToLogin();
+
+        }
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
